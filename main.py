d861278885237b8a8dc15b70fd9c22dfaa803b8f
Stanislav Mitzengendler <asmkors@gmail.com>
2024-08-06 15:31:06 +0300

    working parser but half with 429code

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+import time
 
 URL = {
     'otodom': 'https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/cala-polska?ownerTypeSingleSelect=ALL&viewType=listing',
 }
 
-headers = {'User-Agent': UserAgent().chrome}
+headers = {'User-Agent': UserAgent().random}
 params = {}
 
+a = 0
 async def fetch(session: aiohttp.ClientSession, url: str, params: dict) -> str:
-    async with session.get(url, headers=headers, params=params) as response:
+    async with session.get(url, headers={'User-Agent': UserAgent().random}, params=params) as response:
+        global a
+        a += 1
+        print(response.status, a)
         response = await response.text()
         return response
 
@@ -21,27 +26,18 @@ async def parse_otodom() -> tuple:
         html = await fetch(session, URL.get('otodom'), {})
         soup = BeautifulSoup(html, 'lxml')
 
-        # page_quantity = int(
-        #     soup.find('ul', class_='e1h66krm4 css-iiviho').find_all('li', class_='css-1lclt1h')[-1].text)
-        page_quantity = 609
+        page_quantity = int(
+            soup.find('ul', class_='e1h66krm4 css-iiviho').find_all('li', class_='css-1lclt1h')[-1].text)
+
         tasks = []
         for page_number in range(1, page_quantity + 1):
             tasks.append(asyncio.create_task(fetch(session, URL.get('otodom'), {'page': page_number})))
 
-        print(123)
-        # Запускаем задачи и собираем результаты
-        results = await asyncio.gather(*tasks)
-        print(123)
-        # Обработка результатов
-        asd=[]
-        a = 1
-        for result in results:
-            soup = BeautifulSoup(result, 'lxml').text
-            asd.append(soup + '\n' + str(a))
-            a+=1
-        print(asd)
+
+        for result in await asyncio.gather(*tasks):
+            soup = BeautifulSoup(result, 'lxml')
 
 
-
-
+start_time = time.monotonic()
 asyncio.run(parse_otodom())
+print(round(time.monotonic() - start_time, 1))
