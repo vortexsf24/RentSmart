3038ec7a7f7792ac7f3bc20832c8fdb237ce7a9e
Stanislav Mitzengendler <asmkors@gmail.com>
2024-08-20 10:02:16 +0300

    add automatic json link finder

--- otodom.py
+++ otodom.py
@@ -1,6 +1,6 @@
 # line 50 сделать так чтобы ретерн не None
 # сделать success если все все правильно в бд записалось, Error елси не все, critical если ничего
-# сделать функцию которая автоматически при ошибке 404 находит новую ссылку на файл json
+# сделать json_url переменной окружения и использовать ассерт
 # добавить прокси и при 403 их менять
 
 import time
@@ -16,7 +16,8 @@ from loguru import logger
 from log import logger_setup
 
 PAGE_URL = 'https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/cala-polska'
-JSON_URL = 'https://www.otodom.pl/_next/data/spJgSgMUsdojODn2PoCzO/pl/wyniki/wynajem/mieszkanie/cala-polska.json'
+
+JSON_URL = 'https://www.otodom.pl/_next/data//pl/wyniki/wynajem/mieszkanie/cala-polska.json'
 params = {
     'limit': '72',
     'viewType': 'listing',
@@ -34,6 +35,20 @@ headers = {
 MAX_REQUESTS_ATTEMPTS = 5
 
 
+async def find_json_link(session: aiohttp.ClientSession) -> str:
+    global JSON_URL
+
+    html = await fetch(session, PAGE_URL, params)
+    soup = BeautifulSoup(html, 'lxml')
+
+    page_json = json.loads(soup.find('script', id='__NEXT_DATA__').text)
+
+    unique_link_part = page_json['buildId']
+    print(unique_link_part)
+    JSON_URL = f'https://www.otodom.pl/_next/data/{unique_link_part}/pl/wyniki/wynajem/mieszkanie/cala-polska.json'
+    print(JSON_URL)
+
+
 async def fetch(session: aiohttp.ClientSession, url: str, params: dict, request_attempt: int = 1) -> str | None:
     try:
         async with session.get(url, params=params, headers=headers) as response:
@@ -45,7 +60,8 @@ async def fetch(session: aiohttp.ClientSession, url: str, params: dict, request_
             logger.critical(_failed_request)
 
         elif _failed_request.status == 404:
-            logger.critical(_failed_request)
+            logger.info('JSON link is expired, finding a new one...')
+            await find_json_link(session)
 
         elif _failed_request.status == 429:
             if request_attempt == MAX_REQUESTS_ATTEMPTS:
@@ -80,6 +96,9 @@ async def get_postings() -> int:
         page_quantity = int(
             soup.find('ul', attrs={'data-cy': 'frontend.search.base-pagination.nexus-pagination'}).find_all('li')[-2].text
         )
+
+        # if 404 occurs then it will find a new link
+        test_json_request = await fetch(session, JSON_URL, params)
 
         tasks = []
         for page_number in range(1, page_quantity + 1):
